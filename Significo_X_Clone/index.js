955a25cb5badc7994395b6bae909ccdda8e7f60e
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-03-30 18:13:49 +0530

    added footer animation, bodyColorTHemeChange for every section, locomotiveScroll, animation for testimonials Page, footer sticky reaching bottom, deleted footer image

--- Significo_X_Clone/index.js
+++ Significo_X_Clone/index.js
@@ -1,3 +1,9 @@
+function loco() {
+    (function () {
+        const locomotiveScroll = new LocomotiveScroll();
+    })();
+}
+
 function homePageAnimation() {
     gsap.set(".slidesm", {scale: 5})
 
@@ -70,47 +76,119 @@ function teamPageAnimation() {
 
 function testimonialsPageAnimation() {
     var clutter = "";
-    let testimonials = document.querySelector(".testimonials");
-    testimonials.textContent.split(" ").forEach(function(textElement) {
-        if (textElement === " ") clutter += `<span>&nbsp;</span>`;
-        clutter += `<span>${textElement}</span>`;
-        
+    let testimonialsPara = document.querySelectorAll(".testimonials-para");
+    testimonialsPara.forEach(textContent => {
+        clutter = "";
+        let words = textContent.textContent.split(" ");
+        words.forEach(function(textElement) {
+            if (textElement === " ") clutter += `<span>&nbsp;</span>`;
+            clutter += `<span>${textElement} </span>`;
+        });
+        textContent.innerHTML = clutter;
+    });
+
+    gsap.set(".testimonials-para span", { opacity: 0.1});
+    gsap.set(".testimonials-para span", {
+        scrollTrigger: {
+            trigger: ".testimonials",
+            start: "top 85%",
+            end: "bottom 80%",
+            scrub: 1,
+        },
+        opacity: 1,
+        stagger: .03,
+        ease: Power4,
+    });
+}
+
+function capsulesPageAnimation() {
+    gsap.to(".capsule:nth-child(1)", {
+        scrollTrigger: {
+            trigger: ".capsules",
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1,
+        },
+        y: 0,
+        ease: Power4,
     });
 }
 
 function footerReachedBottom(){
-  // Function to prevent default scrolling behavior
-  function preventScroll(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-  }
-  
-  // Function to toggle scroll functionality
-  function toggleScroll(enable) {
-    if (enable) {
-      window.removeEventListener('wheel', preventScroll, { passive: false });
-      window.removeEventListener('touchmove', preventScroll, { passive: false });
-    } else {
-      window.addEventListener('wheel', preventScroll, { passive: false });
-      window.addEventListener('touchmove', preventScroll, { passive: false });
+    // Function to prevent default scrolling behavior
+    function preventScroll(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
     }
-  }
-  
-  // Function to check if the footer is in the viewport
-  function isFooterInView() {
-    const footer = document.querySelector('.footer');
-    const rect = footer.getBoundingClientRect();
-    return rect.top < window.innerHeight;
-  }
-  
-  window.addEventListener('scroll', () => {
-    // Footer is in view, disable scrolling else enable scroling
-    isFooterInView() ? toggleScroll(false) : toggleScroll(true);
-  });
+    
+    // Function to toggle scroll functionality
+    function toggleScroll(enable) {
+      if (enable) {
+        window.removeEventListener('wheel', preventScroll, { passive: false });
+        window.removeEventListener('touchmove', preventScroll, { passive: false });
+      } else {
+        window.addEventListener('wheel', preventScroll, { passive: false });
+        window.addEventListener('touchmove', preventScroll, { passive: false });
+      }
+    }
+    
+    // Function to check if the footer is in the viewport
+    function isFooterInView() {
+      const footer = document.querySelector('.footer');
+      const rect = footer.getBoundingClientRect();
+      return rect.top < window.innerHeight;
+    }
+    
+    window.addEventListener('scroll', () => {
+      // Footer is in view, disable scrolling else enable scroling
+      isFooterInView() ? toggleScroll(false) : toggleScroll(true);
+    });
 }
 
+function bodyColorThemeChange() {
+    let section = document.querySelectorAll(".section");
+    section.forEach(function(element) {
+        ScrollTrigger.create({
+            trigger: element,
+            start: "top 65%",
+            end: "bottom 50%",
+            onEnter: function() {
+                document.body.setAttribute("theme", element.dataset.color);
+            },
+            onEnterBack: function() {
+                document.body.setAttribute("theme", element.dataset.color);
+            },
+        })
+    });
+}
+
+function significoLogoAnimation() {
+    document.addEventListener("DOMContentLoaded", function() {
+        let options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.5
+        };
+    
+        let observer = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    document.querySelector('.significoLogo').classList.add('animate');
+                }
+            });
+        }, options);
+    
+        observer.observe(document.querySelector('.footer'));
+    });
+}
+
+loco();
 homePageAnimation();
 realPageAnimation();
 teamPageAnimation();
 testimonialsPageAnimation();
+capsulesPageAnimation();
+footerReachedBottom();
+bodyColorThemeChange();
+significoLogoAnimation();
